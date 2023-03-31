8e32fa4b9941552f131bcf5ad0c269b4b40bb776
Isaac <fimbresisaac23@gmail.com>
2023-03-30 21:08:06 -0700

    new controllers

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get()
-  helloworld() {
-    return 'Hello World!';
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this.tasksService.createTask(newTask.title, newTask.description);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param() id: string) {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Put(':id')
+  updateTask(@Param() id: string, @Body() updatedFields: UpdateTaskDto) {
+    return this.tasksService.updateTask(id, updatedFields);
   }
 }
