1442a6f93a4ef2b6903518e654ec288687117909
Isaac <fimbresisaac23@gmail.com>
2023-03-30 21:20:03 -0700

    adding class validations to the dtos

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,13 +1,34 @@
 /* eslint-disable prettier/prettier */
+import { IsString, IsNotEmpty, IsIn, MinLength, IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks.entity';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+
+  @IsString()
   description: string;
 }
 
 export class UpdateTaskDto {
+  @IsString()
+  @MinLength(3)
+  @IsNotEmpty()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsIn([
+    TaskStatus.DONE,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.PENDING
+  ])
+  @IsOptional()
   status?: TaskStatus;
 }
